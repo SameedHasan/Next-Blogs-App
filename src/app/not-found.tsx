cb733b0f66ddf92b375fc05417caabf3c74122b8
Sameed Hasan <sameedh41@gmail.com>
2024-08-08 15:16:24 +0500

    not found page setting

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 export default function NotFound() {
   return (
-    <div className={"container"}>
-      <h1 className={"title"}>404</h1>
-      <p className={"message"}>
+    <div className="container">
+      <h1 className="title">404</h1>
+      <p className="message">
         Oops! The page you’re looking for doesn’t exist.
       </p>
-      <p className={"description"}>
+      <p className="description">
         It looks like the page you’re trying to reach is either missing or has
         been moved.
       </p>
       <Link href="/">
-        <p className={"homeLink"}>Go back to Home</p>
+        <p className="homeLink">Go back to Home</p>
       </Link>
     </div>
   );
